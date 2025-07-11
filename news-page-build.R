2e4ab9d3cf6ed057db2f71f3a32ed57305bb2fa6
jaytimm <jason.timm@zohomail.com>
2025-07-11 15:19:29 -0600

    ffs

--- news-page-build.R
+++ news-page-build.R
@@ -1,4 +1,4 @@
-# Standalone script: web search, scrape, and paginated news HTML generation
+# Standalone script: web search, scrape, and paginated news Rmd generation
 # Requires: textpress, stringr
 # setwd("/home/jtimm/Dropbox/GitHub/blog")
 library(textpress)
@@ -28,41 +28,29 @@ bingesults <- textpress::web_search(search_term = sterm,
 urls <- unique(c(bingesults$raw_url, yresults$raw_url, ddgesults$raw_url))
 txt <- textpress::web_scrape_urls(x = urls, cores = 3)
 
-# 3. Paginated HTML news page generation
-# Use the main site template for consistent styling
-template <- readLines("assets/template.html")
-
-fill_template <- function(title, date, body) {
-  t <- template
-  t <- gsub("\\$title\\$", title, t)
-  t <- gsub("\\$date\\$", date, t)
-  t <- gsub("\\$body\\$", body, t)
-  paste(t, collapse = "\n")
-}
-
-# Prepare news items (assume txt has columns: h1_title, url, date)
 txt$date[is.na(txt$date)] <- ""
 txt$h1_title[is.na(txt$h1_title)] <- txt$url[is.na(txt$h1_title)]
 
-# No pagination: show all news on a single page
-page_txt <- txt
-
-# Build news list HTML
+# Build news list as markdown
 news_items <- paste0(
-  "<li>",
-  ifelse(page_txt$date != "", paste0("<span style='color:gray;font-size:90%'>", page_txt$date, "</span> - "), ""),
-  "<a href='", page_txt$url, "' target='_blank'>", page_txt$h1_title, "</a>",
-  "</li>"
-)
-news_list <- paste0("<ul>\n", paste(news_items, collapse = "\n"), "\n</ul>")
-
-# Fill template
-html <- fill_template(
-  title = "News",
-  date = format(Sys.Date()),
-  body = paste0("<h2>News</h2>", news_list)
+  "- ",
+  ifelse(txt$date != "", paste0(txt$date, " - "), ""),
+  "[", txt$h1_title, "](", txt$url, ")"
 )
 
-# Write file
-fname <- "news/index.html"
-writeLines(html, fname) 
+# Compose Rmd content
+news_rmd <- c(
+  "---",
+  "title: 'News'",
+  "output:",
+  "  html_document:",
+  "    template: ../assets/template.html",
+  "    css: ../assets/style.css",
+  "---",
+  "",
+  "## Latest News",
+  "",
+  news_items
+)
+
+writeLines(news_rmd, "news/index.Rmd") 
